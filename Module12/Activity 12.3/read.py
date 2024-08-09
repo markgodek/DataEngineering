4b0af60ed4269209c291e3d3b05080e57b2bb123
markgodek <markgodek@gmail.com>
2024-08-08 23:12:52 -0400

    Finished Activity 12.3

--- Module12/Activity 12.3/read.py
+++ Module12/Activity 12.3/read.py
@@ -5,5 +5,17 @@ import redis
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 # read items
-for item in r.lrange('nums', 0, -1):
-    print(item)
+#for item in r.lrange('nums', 0, -1):
+#    print(item)
+
+# get value based on key
+capitalOfItaly = r.get('Italy')
+print('Capital of Italy: ', capitalOfItaly)
+
+# print all key-value pairs
+keys = r.keys()
+for key in keys:
+    print('Key:', key,'-','Value:',r.get(key))
+
+# clean up from the boilerplate code
+#r.delete('nums')
--- Module12/Activity 12.3/write.py
+++ Module12/Activity 12.3/write.py
@@ -8,3 +8,5 @@ r = redis.Redis(host='localhost', port=6379, db=0)
 # write to server
 t = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 r.rpush('nums',t)
+
+r.mset({"Italy": "Rome", "France": "Paris"})
